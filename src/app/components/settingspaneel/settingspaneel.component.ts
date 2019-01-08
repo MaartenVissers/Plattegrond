754de2e95dc8d3bfb284cdb3b7912c9eaebbc1e5
maartenvissers <maarten.vissers@student.kdg.be>
2019-01-09 00:57:59 +0100

    localstorage toegevoegd

--- src/app/components/settingspaneel/settingspaneel.component.ts
+++ src/app/components/settingspaneel/settingspaneel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SettingsService} from '../../services/settings.service';
 import {Settings} from '../../model/settings';
 
@@ -7,53 +7,80 @@ import {Settings} from '../../model/settings';
   templateUrl: './settingspaneel.component.html',
   styleUrls: ['./settingspaneel.component.css']
 })
-export class SettingspaneelComponent implements OnInit {
+export class SettingspaneelComponent {
   settings: Settings;
 
-  constructor(private settingsService: SettingsService) { }
+  constructor(private settingsService: SettingsService) {
+    if (this.getSettings() === null){
+      this.settings = this.getSettings();
+      this.setLocalStorageSettings(this.settings);
+    } else {
+      this.settings = this.getSettings();
+    }
 
-  ngOnInit() {
-    this.settingsService.currentSettings.subscribe(settings => this.settings = settings);
+    this.settingsService.changeSettings(this.settings);
+
+
   }
 
-  seeNaam(status){
-    if (status === 'checked'){
+  getSettings(): Settings {
+    let localStorageItem = JSON.parse(localStorage.getItem('settings'));
+    return localStorageItem == null ? new Settings() : localStorageItem.settings;
+  }
+
+  setLocalStorageSettings(settings: Settings): void {
+    localStorage.setItem('settings', JSON.stringify({settings: settings}));
+  }
+
+
+  seeNaam(status) {
+    if (status === 'checked') {
       this.settings.name = true;
-    }else{
+    } else {
       this.settings.name = false;
     }
+    this.setLocalStorageSettings(this.settings);
+    this.settingsService.changeSettings(this.settings);
   }
 
-  seeType(status){
-    if (status === 'checked'){
+  seeType(status) {
+    if (status === 'checked') {
       this.settings.type = true;
-    }else{
+    } else {
       this.settings.type = false;
     }
+    this.setLocalStorageSettings(this.settings);
+    this.settingsService.changeSettings(this.settings);
   }
 
-  seeBezet(status){
-    if (status === 'checked'){
+  seeBezet(status) {
+    if (status === 'checked') {
       this.settings.bezet = true;
-    }else{
+    } else {
       this.settings.bezet = false;
     }
+    this.setLocalStorageSettings(this.settings);
+    this.settingsService.changeSettings(this.settings);
   }
 
-  seeBeamer(status){
-    if (status === 'checked'){
+  seeBeamer(status) {
+    if (status === 'checked') {
       this.settings.beamer = true;
-    }else{
+    } else {
       this.settings.beamer = false;
     }
+    this.setLocalStorageSettings(this.settings);
+    this.settingsService.changeSettings(this.settings);
   }
 
-  seeCapaciteit(status){
-    if (status === 'checked'){
+  seeCapaciteit(status) {
+    if (status === 'checked') {
       this.settings.capaciteit = true;
-    }else{
+    } else {
       this.settings.capaciteit = false;
     }
+    this.setLocalStorageSettings(this.settings);
+    this.settingsService.changeSettings(this.settings);
   }
 
 }
